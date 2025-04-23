b77e16322cedb7981d67b88118a91d49a7f65c7e
Luis Guzman <lumigu.dev@gmail.com>
2025-04-22 23:24:35 -0500

    ✨ Add Territory enumeration to define available territories

---
+++ app/models/enum.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class Territory(Enum):
+    TERRITORY_1 = 1
+    TERRITORY_2 = 2
+    TERRITORY_3 = 3
+    TERRITORY_4 = 4
+    TERRITORY_5 = 5
+    TERRITORY_6 = 6
+    TERRITORY_7 = 7
+    TERRITORY_8 = 8
+    TERRITORY_9 = 9
+    TERRITORY_10 = 10
